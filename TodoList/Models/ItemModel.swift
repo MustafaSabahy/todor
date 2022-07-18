86ab547a02e29e318e2fb465209a4c90011d3e4e
mustafasabahy <m.elsabahy96@gmail.com>
2022-07-18 23:25:13 +0200

    Initial Commit

---
+++ TodoList/Models/ItemModel.swift
@@ -0,0 +1,26 @@
+//
+//  ItemModel.swift
+//  TodoList
+//
+//  Created by  mustafa sabahy on 02/05/2022.
+
+import Foundation
+
+// Immutable Struct has only 'let' constants
+struct ItemModel: Identifiable, Codable {
+    let id: String
+    let title: String
+    let isCompleted: Bool
+    
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    func updateCompletion() -> ItemModel {
+        return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+    }
+    
+}
+
